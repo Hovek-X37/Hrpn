e4a76de634d70f90112ef1a31f73e0664f2cc5b8
Nick Phillips <61893985+Hovek-X37@users.noreply.github.com>
2024-11-10 13:50:33 -0500

    Add files via upload
    Initial file upload

---
+++ hrpn.py
@@ -0,0 +1,296 @@
+from pathlib import Path
+from settings import HRPNS
+import argparse
+import json
+import os
+import math
+import readline
+
+
+class HRPNApp():
+    stack = []
+    regpath = Path('registry.json')
+
+    def __init__(self, args):
+        self.settings = {}
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.hsg = HRPNS(args)
+        self.settings = self.hsg.get_settings()
+        self.registry = self.hsg.get_registry()
+        self.header = '\033[95m'
+        self.okblue = '\033[94m'
+        self.okcyan = '\033[96m'
+        self.okgreen = '\033[92m'
+        self.warnc = '\033[93m'
+        self.fail = '\033[91m'
+        self.endc = '\033[0m'
+        self.bold = '\033[1m'
+        self.underline = '\033[4m'
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def check_length(self, reqlength):
+        if len(self.stack) < reqlength:
+            return False
+        return True
+
+    def error(self, message):
+        self.clear_screen()
+        print(self.fail + message + self.endc)
+
+    def result(self, message):
+        print(self.okgreen + message + self.endc)
+
+    def warn(self, message):
+        self.clear_screen()
+        print(self.warnc + message + self.endc)
+
+    def change_sign(self):
+        self.clear_screen()
+        if len(self.stack) < 1:
+            self.warn("Not enough items in stack")
+            return
+        x = self.stack.pop()
+        if x < 0:
+            self.stack.append(abs(x))
+        else:
+            self.stack.append(-abs(x))
+
+    def swap_xy(self):
+        self.clear_screen()
+        if len(self.stack) < 2:
+            self.warn("Not enough items in stack")
+            return
+        x = self.stack.pop()
+        y = self.stack.pop()
+        self.stack.append(x)
+        self.stack.append(y)
+
+    def drop(self):
+        self.clear_screen()
+        if len(self.stack) == 0:
+            self.warn("Not enough items in stack")
+            return
+        self.stack.pop()
+
+    def clear(self):
+        self.clear_screen()
+        self.stack.clear()
+
+    def add(self):
+        self.clear_screen()
+        if len(self.stack) < 2:
+            self.warn("Not enough items in stack")
+            return
+        y = self.stack.pop()
+        x = self.stack.pop()
+        self.stack.append(x + y)
+
+    def subtract(self):
+        self.clear_screen()
+        if len(self.stack) < 2:
+            self.warn("Not enough items in stack")
+            return
+        y = self.stack.pop()
+        x = self.stack.pop()
+        self.stack.append(x - y)
+
+    def multiply(self):
+        self.clear_screen()
+        if len(self.stack) < 2:
+            self.warn("Not enough items in stack")
+            return
+        y = self.stack.pop()
+        x = self.stack.pop()
+        try:
+            self.stack.append(x * y)
+        except OverflowError:
+            self.error("Result too large")
+
+    def divide(self):
+        self.clear_screen()
+        if len(self.stack) < 2:
+            self.warn("Not enough items in stack")
+            return
+        y = self.stack.pop()
+        x = self.stack.pop()
+        if y == 0:
+            self.error("Cannot divide by zero")
+            return
+        self.stack.append(x / y)
+
+    def push(self, arg):
+        self.clear_screen()
+        self.arg = arg
+        if len(self.stack) == self.settings['stacksize']:
+            self.warn("Stack is full")
+        else:
+            try:
+                self.stack.append(float(arg))
+            except ValueError:
+                self.warn("Invalid input")
+
+    def pi(self):
+        self.clear_screen()
+        if len(self.stack) == self.settings['stacksize']:
+            self.warn("Stack is full")
+            return
+        self.stack.append(math.pi)
+
+    def store_number(self, key):
+        self.clear_screen()
+        if len(self.stack) == 0:
+            self.warn("Stack is empty")
+            return
+        self.registry[key] = self.stack[-1]
+        if self.hsg.cmd_line() is False and self.hsg.ret_setting('registry') is True:
+            self.regpath.write_text(json.dumps(self.registry))
+        self.result(f"Number stored to key {key}")
+
+    def changesettings(self):
+        self.clear_screen()
+        if self.hsg.cmd_line() is True:
+            self.warn("Cannot change settings in command line mode")
+            return
+        self.hsg.change_app_settings()
+        self.settings = self.hsg.get_settings()
+        return
+
+    def recall_number(self, key):
+        self.clear_screen()
+        if key not in self.registry:
+            self.warn(f"No number stored in key {key}")
+            return
+        self.stack.append(self.registry[key])
+        self.result(f"Number recalled from key {key}")
+
+    def show_stack(self, bypass=False):
+        decs = self.hsg.ret_setting('decimalplaces')
+        if self.hsg.ret_setting('hidestack') is False or bypass is True:
+            if self.hsg.ret_setting('orientation') == "v":
+                print("Stack:")
+                for i in range(len(self.stack)):
+                    if decs == 0:
+                        print(self.stack[i])
+                    else:
+                        print(f'{self.stack[i]:.{decs}f}')
+            else:
+                print("Stack: [", end=" ")
+                if decs == 0:
+                    print(', '.join(str(q) for q in self.stack), end=" ")
+                else:
+                    print(', '.join(f'{q:.{decs}f}' for q in self.stack), end=" ")
+                print("]")
+        else:
+            return
+
+    def last_number(self, digits=8):
+        self.clear_screen()
+        if len(self.stack) == 0:
+            self.warn("Stack is empty")
+            return
+        if digits == "f":
+            lastnum = self.stack[-1]
+        else:
+            try:
+                digits = int(digits)
+            except ValueError:
+                self.warn("Invalid input")
+                return
+            digit = int(digits)
+            lastnum = round(self.stack[-1], digit)
+        self.result(f"Last number: {lastnum}")
+
+    def show_header(self):
+        if self.hsg.ret_setting('hideheader') is False:
+            print(self.header + self.bold + self.underline + "HRPN Calculator" + self.endc)
+            print("Enter a command or number to push to the stack")
+            print("Commands: +, -, *, /, c, d, s, x, n, p, q, sto[key], rcl[key], settings")
+        else:
+            return
+
+    def show_help(self):
+        self.clear_screen()
+        print(self.header + "HRPN Calculator Help" + self.endc)
+        print("Commands:")
+        print("+: Add the top two numbers on the stack")
+        print("-: Subtract the top two numbers on the stack")
+        print("*: Multiply the top two numbers on the stack")
+        print("/: Divide the top two numbers on the stack")
+        print("c: Clear the stack")
+        print("d: Drop the top number from the stack")
+        print("s: Swap the top two numbers on the stack")
+        print("x: Change the sign of the top number on the stack")
+        print("n: Show the stack")
+        print("p: Push pi to the stack")
+        print("sto[key]: Store the top number on the stack to the registry")
+        print("rnd[digits]: Show the last number on the stack with the specified number of digits")
+        print("rcl[key]: Recall a number from the registry")
+        print("settings: Change the settings")
+        print("q: Quit the calculator")
+
+    def show_about(self):
+        self.clear_screen()
+        print(self.header + "HRPN Calculator" + self.endc)
+        print("Version 0.1")
+        print("Written by: Hovek")
+        print("Type help for a list of commands")
+
+    def run(self):
+        while True:
+            self.show_header()
+            self.show_stack()
+            command = input(self.settings['prompt'])
+            if command == "+":
+                self.add()
+            elif command == "-":
+                self.subtract()
+            elif command == "*":
+                self.multiply()
+            elif command == "/":
+                self.divide()
+                command = command[1:9]
+            elif command == "c":
+                self.clear()
+            elif command == "d":
+                self.drop()
+            elif command == "s":
+                self.swap_xy()
+            elif command == "p":
+                self.pi()
+            elif command == "x":
+                self.change_sign()
+            elif command == "n":
+                self.show_stack(True)
+            elif command == "q":
+                break
+            elif command.startswith('sto'):
+                self.store_number(command[3:4])
+            elif command.startswith('rnd'):
+                self.last_number(command[3:6])
+            elif command.startswith('rcl'):
+                self.recall_number(command[3:4])
+            elif command == "set":
+                self.changesettings()
+            elif command == "help":
+                self.show_help()
+            elif command == "about":
+                self.show_about()
+            else:
+                self.push(command)
+
+
+parser = argparse.ArgumentParser(description="HRPN Calculator")
+parser.add_argument("-p", "--prompt", type=str, help="Set the prompt")
+parser.add_argument("-s", "--stacksize", type=int, help="Set the stack size")
+parser.add_argument("-d", "--decimalplaces", type=int, help="Set the number of decimal places")
+parser.add_argument("-hh", "--hideheader", action="store_true", help="Hide the header")
+parser.add_argument("-sh", "--hidestack", action="store_true", help="Hide the stack")
+parser.add_argument("-o", "--orientation", type=str, help="Set the stack orientation", choices=["v", "h"])
+parser.add_argument("-c", "--commandline", action="store_true", help="use command line arguments instead of settings file", default=False)
+parser.add_argument("-f", "--file", type=str, help="Settings file", default="settings.json")
+parser.add_argument("-nr", "--noregistry", action="store_true", help="Don't use Registry file", default=False)
+args = parser.parse_args()
+HRPN = HRPNApp(args)
+HRPN.run()
---
+++ settings.py
@@ -0,0 +1,194 @@
+from pathlib import Path    # Path manipulation
+import json                 # JSON file handling
+
+
+class HRPNS():
+
+    def __init__(self, args):
+        self.settings = {}
+        self.registry = {}
+        if args.commandline is True:
+            self.cmdline = True
+            if args.prompt is None:
+                self.settings.update({'prompt': ':'})
+            else:
+                self.settings.update({'prompt': args.prompt})
+            if args.stacksize is None:
+                self.settings.update({'stacksize': int(4)})
+            else:
+                self.settings.update({'stacksize': args.stacksize})
+            if args.decimalplaces is None:
+                self.settings.update({'decimalplaces': int(2)})
+            else:
+                self.settings.update({'decimalplaces': args.decimalplaces})
+            self.settings.update({'hideheader': args.hideheader})
+            self.settings.update({'hidestack': args.hidestack})
+            self.settings.update({'registry': False})
+        else:
+            self.cmdline = False
+            self.path = Path(args.file)
+            self.regpath = Path('registry.json')
+            if self.path.exists():
+                self.settings = json.loads(self.path.read_text())
+                if args.prompt is not None:
+                    self.settings.update({'prompt': args.prompt})
+                if args.stacksize is not None:
+                    self.settings.update({'stacksize': args.stacksize})
+                if args.decimalplaces is not None:
+                    self.settings.update({'decimalplaces': args.decimalplaces})
+                if args.hideheader is not None:
+                    self.settings.update({'hideheader': args.hideheader})
+                if args.hidestack is not None:
+                    self.settings.update({'hidestack': args.hidestack})
+                if args.orientation is not None:
+                    self.settings.update({'orientation': args.orientation})
+                else:
+                    self.settings.update({'orientation': 'v'})
+                if args.noregistry is True:
+                    self.settings.update({'registry': False})
+                else:
+                    self.settings.update({'registry': True})
+            else:
+                self.settings.update({'prompt': ':'})
+                self.settings.update({'stacksize': int(4)})
+                self.settings.update({'decimalplaces': int(2)})
+                self.settings.update({'hideheader': False})
+                self.settings.update({'hidestack': False})
+                self.settings.update({'registry': True})
+                self.path.write_text(json.dumps(self.settings))
+
+    def change_app_settings(self):
+        self.clear_screen()
+        while True:
+            print("Current settings:")
+            for key, value in self.settings.items():
+                print(f"{key}: {value}")
+            expl = ""
+            expl = "([p]rompt, [ss]stacksize, [dp]decimalplaces, [hh]hideheader \
+([p]rompt, [ss]stacksize, [dp]decimalplaces, [hh]hideheader)\n"
+            expl = expl + "Enter the setting you would like to change, or q to quit:"
+            which = input(expl)
+            if which == "p":
+                self.change_prompt()
+            elif which == "ss":
+                self.change_stacksize()
+            elif which == "dp":
+                self.change_decimalplaces()
+            elif which == "hh":
+                self.change_hideheader()
+            elif which == "sh":
+                self.change_hidestack()
+            elif which == "so":
+                self.change_orientation()
+            elif which == "r":
+                self.change_registry()
+            elif which == "s":
+                self.save_settings()
+            elif which == "q":
+                self.clear_screen()
+                return
+            else:
+                print("Invalid input")
+
+    def change_prompt(self):
+        self.clear_screen()
+        prompt = input("Enter new prompt: ")
+        self.settings.update({'prompt': prompt})
+        return prompt
+
+    def change_stacksize(self):
+        self.clear_screen()
+        stacksize = input("Enter new stack size: ")
+        self.settings.update({'stacksize': int(stacksize)})
+        return stacksize
+
+    def change_decimalplaces(self):
+        self.clear_screen()
+        while True:
+            decimalplaces = input("Enter new decimal places,0 disables rounding:")
+            if decimalplaces.lstrip("-").isdigit():
+                self.settings.update({'decimalplaces': int(decimalplaces)})
+                return decimalplaces
+            else:
+                print("Invalid input")
+
+    def change_hideheader(self):
+        self.clear_screen()
+        while True:
+            hideheader = input("Hide header? (y/n): ")
+            if hideheader == "y":
+                hideheader = True
+                self.settings.update({'hideheader': hideheader})
+                return hideheader
+            elif hideheader == "n":
+                hideheader = False
+                self.settings.update({'hideheader': hideheader})
+                return hideheader
+            else:
+                print("Invalid input")
+
+    def change_orientation(self):
+        self.clear_screen()
+        while True:
+            if 'orientation' in self.settings:
+                print(f"Current orientation: {self.settings['orientation']}")
+            else:
+                print("Orientation not set")
+            orientation = input("Orientation? (v/h): ")
+            if orientation == "v":
+                self.settings.update({'orientation': orientation})
+                return orientation
+            elif orientation == "h":
+                self.settings.update({'orientation': orientation})
+                return orientation
+            else:
+                print("Invalid input")
+
+    def change_hidestack(self):
+        self.clear_screen()
+        while True:
+            hidestack = input("Hide stack? (y/n): ")
+            if hidestack == "y":
+                hidestack = True
+                self.settings.update({'hidestack': hidestack})
+                return hidestack
+            elif hidestack == "n":
+                hidestack = False
+                self.settings.update({'hidestack': hidestack})
+                return hidestack
+            else:
+                print("Invalid input")
+
+    def change_registry(self):
+        self.clear_screen()
+        while True:
+            registry = input("Use registry file? (y/n): ")
+            if registry == "y":
+                registry = True
+                self.settings.update({'registry': registry})
+                return registry
+            elif registry == "n":
+                registry = False
+                self.settings.update({'registry': registry})
+                return registry
+            else:
+                print("Invalid input")
+
+    def clear_screen(self):
+        print("\033[H\033[J")
+
+    def save_settings(self):
+        self.path.write_text(json.dumps(self.settings))
+        return
+
+    def get_settings(self):
+        return self.settings
+
+    def ret_setting(self, key):
+        return self.settings[key]
+
+    def get_registry(self):
+        return self.registry
+
+    def cmd_line(self):
+        return self.cmdline
